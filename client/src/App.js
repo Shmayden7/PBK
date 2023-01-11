2297747136f71b63d42ea08bd5b3a8636f1f76ad
Ayden Cauchi <ayden@modimages.com>
2023-01-11 17:12:30 -0500

    01_01_11, added sound files, started front end, added stycomp and material UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
 import React, {useState, useEffect} from 'react'
+import './App.css'
+import { Grid, Paper, Box } from '@mui/material';
 
 function App(){
   const [data, setData] = useState([{}])
@@ -14,14 +16,17 @@ function App(){
   }, [])
 
   return (
-    <div>
-      {(typeof data.members === 'undefined') ? (
-        <p>Loading...</p>
-      ): (
-        data.members.map((member, i) => (
-          <p key={i}>{member}</p>
-        ))
-      )}
+    <div className="App">
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={0} alignItems='center' align='center'>
+            <Grid item xs={12} sm={6} md={3}>
+              <Paper>A</Paper>
+            </Grid>
+            <Grid item xs={12} sm={6} md={3}>
+              <Paper>B</Paper>
+            </Grid>
+        </Grid>
+      </Box>
     </div>
   )
 }
--- flask-server/server.py
+++ flask-server/server.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 @app.route("/members")
 def members(): 
     return {"members": ["Member1", "Member1", "Member1"]}
+    
+@app.route("/members")
+def members(): 
+    return {"members": ["Member1", "Member1", "Member1"]}
 
 if __name__ == "__main__":
         app.run(debug=True)
